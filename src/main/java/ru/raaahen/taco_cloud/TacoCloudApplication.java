a679c262c8bea15eb76f4e0cde6527a65f0c46a4
sgzh1 <ya.superkesa@gmail.com>
2024-10-13 13:24:25 +0300

    spring project initialized

---
+++ src/main/java/ru/raaahen/taco_cloud/TacoCloudApplication.java
@@ -0,0 +1,13 @@
+package ru.raaahen.taco_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+
+}
